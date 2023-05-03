88be9fc0d023a2d8dfe63ab69d46bb51426f9b14
nyg159 <nyg159@gmail.com>
2023-05-03 13:43:40 +0900

    first

---
+++ src/test/java/awc/test/awstest/AwstestApplicationTests.java
@@ -0,0 +1,13 @@
+package awc.test.awstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwstestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
